3d7f4a990da33a8e3f957a0d32f0a11e0f23cd0e
samoprogramuje <samuel1820@gmail.com>
2024-06-12 09:14:50 +0200

    add login

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+
+    {
+        $this->denyAccessUnlessGranted(attribute:"Is_AUTHETIFICATED_FULLY");
+
+        /**  @var User $user */
+        $user = $this->getUser();
+
+        return match ($user->isVerified()){
+            true => $this->render(view: "admin/index.html.twig"),
+            false => $this->render(view: "admin/verify-email.html.twig")
+        };
+
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
